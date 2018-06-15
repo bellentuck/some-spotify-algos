11279d9882c3b583a4da7be6b14f59e98638ad75
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-15 17:07:59 -0400

    sortByStrings specs passing

--- algos/sortByStrings.js
+++ algos/sortByStrings.js
@@ -1,1 +1,31 @@
-export default () => {};
+const hashChars = str => {
+  const chars = {};
+  for (const char of str[Symbol.iterator]()) {
+    if (chars.hasOwnProperty(char)) chars[char] += 1;
+    else chars[char] = 1;
+  }
+  return chars;
+}
+
+const composeSortedStr = (str, target, charsHash) => {
+  let sortedStr = '';
+  let sortedStrLength = 0;
+  for (const char of target[Symbol.iterator]()) {
+    if (charsHash.hasOwnProperty(char)) {
+      while (charsHash[char]) {
+        sortedStr += char;
+        sortedStrLength++;
+        charsHash[char]--;
+      }
+    }
+    if (sortedStrLength === str.length) break;
+  }
+  return sortedStr;
+}
+
+export default (str, target) => {
+  if (!str || !target || typeof str !== 'string' || typeof target !== 'string') {
+    throw new Error('This function takes 2 inputs which must both be strings.')
+  }
+  return composeSortedStr(str, target, hashChars(str));
+};
--- tests/sortByStrings.test.js
+++ tests/sortByStrings.test.js
@@ -3,9 +3,15 @@
 import { expect } from 'chai';
 import { sort } from '../index';
 
-describe('The `sortByStrings` algorithm', () => {
+describe.only('The `sortByStrings` algorithm', () => {
   let s, t;
-  it('Takes two arguments, both strings');
+  it('Takes two arguments, both strings', () => {
+    const err = 'This function takes 2 inputs which must both be strings.';
+    expect(() => sort('a', 'b')).not.to.throw();
+    expect(() => sort('a', 1)).to.throw(err);
+    expect(() => sort('a')).to.throw(err);
+    expect(() => sort()).to.throw(err);
+  });
   it('Sorts the letters in a string `s` by the order they occur in the string `t` (where `t` will not have repetitive characters)', () => {
     s = 'weather';
     t = 'therapyw';
