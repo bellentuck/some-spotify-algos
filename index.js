413f00ea0195632537282f49e61fa24cd8227667
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-15 11:24:32 -0400

    algos boilerplate

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as sort } from './algos/sortByStrings';
+export { default as decode } from './algos/decodeStrings';
+export { default as change } from './algos/changePossibilities';
