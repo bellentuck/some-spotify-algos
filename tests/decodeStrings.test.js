ee52aa62f62743191d7ba8bcef76c3abff061788
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-16 13:05:23 -0400

    standardized test suites; all specs passing

--- tests/decodeStrings.test.js
+++ tests/decodeStrings.test.js
@@ -4,18 +4,24 @@ import { expect } from 'chai';
 import { decode } from '../index';
 
 describe('The `decodeStrings` algorithm. The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is repeated exactly k times. (Note: k is guaranteed to be a positive integer.)', () => {
-  let s;
-  it('Takes a single argument, a string', () => {
-    const err = 'This function takes 1 input which must be a string.';
-    expect(() => decode('a')).not.to.throw();
-    expect(() => decode(1)).to.throw(err);
-    expect(() => decode()).to.throw(err);
-  });
-  it('Given an encoded string, return its corresponding decoded string', () => {
-    s = '4[ab]';
-    expect(decode(s)).to.equal('abababab');
 
-    s = '2[b3[a]]';
-    expect(decode(s)).to.equal('baaabaaa');
+  describe('Inputs', () => {
+    it('Takes a single argument, a string', () => {
+      const err = 'This function takes 1 input which must be a string.';
+      expect(() => decode('a')).not.to.throw();
+      expect(() => decode(1)).to.throw(err);
+      expect(() => decode()).to.throw(err);
+    });
+  });
+
+  describe('Outputs', () => {
+    it('Given an encoded string, return its corresponding decoded string', () => {
+      let s;
+      s = '4[ab]';
+      expect(decode(s)).to.equal('abababab');
+
+      s = '2[b3[a]]';
+      expect(decode(s)).to.equal('baaabaaa');
+    });
   });
 });
--- tests/sortByStrings.test.js
+++ tests/sortByStrings.test.js
@@ -4,21 +4,27 @@ import { expect } from 'chai';
 import { sort } from '../index';
 
 describe('The `sortByStrings` algorithm', () => {
-  let s, t;
-  it('Takes two arguments, both strings', () => {
-    const err = 'This function takes 2 inputs which must both be strings.';
-    expect(() => sort('a', 'b')).not.to.throw();
-    expect(() => sort('a', 1)).to.throw(err);
-    expect(() => sort('a')).to.throw(err);
-    expect(() => sort()).to.throw(err);
-  });
-  it('Sorts the letters in a string `s` by the order they occur in the string `t` (where `t` will not have repetitive characters)', () => {
-    s = 'weather';
-    t = 'therapyw';
-    expect(sort(s, t)).to.equal('theeraw');
 
-    s = 'good';
-    t = 'odg';
-    expect(sort(s, t)).to.equal('oodg');
+  describe('Inputs', () => {
+    it('Takes two arguments, both strings', () => {
+      const err = 'This function takes 2 inputs which must both be strings.';
+      expect(() => sort('a', 'b')).not.to.throw();
+      expect(() => sort('a', 1)).to.throw(err);
+      expect(() => sort('a')).to.throw(err);
+      expect(() => sort()).to.throw(err);
+    });
+  });
+
+  describe('Outputs', () => {
+    it('Sorts the letters in a string `s` by the order they occur in the string `t` (where `t` will not have repetitive characters)', () => {
+      let s, t;
+      s = 'weather';
+      t = 'therapyw';
+      expect(sort(s, t)).to.equal('theeraw');
+
+      s = 'good';
+      t = 'odg';
+      expect(sort(s, t)).to.equal('oodg');
+    });
   });
 });
