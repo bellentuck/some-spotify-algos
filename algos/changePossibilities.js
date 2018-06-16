3fc5b9c7ccb187a613a98f9d791a9a3b2b27cc06
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-16 13:17:49 -0400

    fix spacing

--- algos/changePossibilities.js
+++ algos/changePossibilities.js
@@ -1,21 +1,21 @@
 const changePossibilities = (total, denoms) => {
-	// Initialize an array of size total + 1, to dynamically keep track
-	// of change possibilities for the whole number interval [0, total+1].
+  // Initialize an array of size total + 1, to dynamically keep track
+  // of change possibilities for the whole number interval [0, total+1].
   const ways = Array(total + 1).fill(0);
   ways[0] = 1; // Base case: 1 way to make nothing: don't use anything
 
-	for (const denom of denoms) { // Iterate through denominations available to us
-		for (let amt = 1; amt < ways.length; amt++) {
+  for (const denom of denoms) { // Iterate through denominations available to us
+    for (let amt = 1; amt < ways.length; amt++) {
       // Iterate through the natural number interval of change possibilities (0, total+1].
-		  if (denom <= amt) { // If denom > amt, no change possibilities
+      if (denom <= amt) { // If denom > amt, no change possibilities
         ways[amt] += ways[amt - denom];
         // However many ways there are to make amt - denom, we can add 1 denom to make amt.
-				// Because we are iterating over possible amts, we can track all denom multiples <= total.
-			}
-		}
+        // Because we are iterating over possible amts, we can track all denom multiples <= total.
+      }
+    }
   }
 
-	return ways[total];
+  return ways[total];
 }
 
 
