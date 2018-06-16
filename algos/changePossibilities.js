a3c8c9d9f88ddebdac574b70498cff7efc97ca04
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-16 13:00:20 -0400

    changePossibilities specs passing

--- algos/changePossibilities.js
+++ algos/changePossibilities.js
@@ -1,25 +1,27 @@
+const changePossibilities = (total, denoms) => {
+	// Initialize an array of size total + 1, to dynamically keep track
+	// of change possibilities for the whole number interval [0, total+1].
+  const ways = Array(total + 1).fill(0);
+  ways[0] = 1; // Base case: 1 way to make nothing: don't use anything
+
+	for (const denom of denoms) { // Iterate through denominations available to us
+		for (let amt = 1; amt < ways.length; amt++) {
+      // Iterate through the natural number interval of change possibilities (0, total+1].
+		  if (denom <= amt) { // If denom > amt, no change possibilities
+        ways[amt] += ways[amt - denom];
+        // However many ways there are to make amt - denom, we can add 1 denom to make amt.
+				// Because we are iterating over possible amts, we can track all denom multiples <= total.
+			}
+		}
+  }
+
+	return ways[total];
+}
+
+
 export default (amt, denoms) => {
   if (isNaN(Number(amt)) || !Array.isArray(denoms) || denoms.some(el => isNaN(Number(el)))) {
     throw new Error('This function takes 2 inputs which must be a number and an array of numbers, respectively.');
   }
-
-  const seenDenoms = [];
-  let possibilities = 0;
-  const sortedDenoms = denoms.sort((a, b) => a > b);
-  for (let i = 0; i < sortedDenoms.length; i++) {
-    if (sortedDenoms[i] > amt) break;
-    if (sortedDenoms[i] === amt) {
-      possibilities++;
-      break;
-    }
-
-    if (amt % sortedDenoms[i] === 0) possibilities++;
-
-    let multiples = 1;
-
-
-
-    seenDenoms.push(sortedDenoms[i]);
-
-  }
+  return changePossibilities(amt, denoms);
 }
--- tests/changePossibilities.test.js
+++ tests/changePossibilities.test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { change } from '../index';
 
 describe('The `changePossibilities` algorithm. (Your quirky boss collects rare, old coins. They found out you\'re a programmer and asked you to solve something they\'ve been wondering for a long time. #inspiringTangents)', () => {
-  let amount, denominations;
+
   it('Takes two arguments, a number and an array of numbers', () => {
     const err = 'This function takes 2 inputs which must be a number and an array of numbers, respectively.';
     expect(() => change(1, [2, 3, 4])).not.to.throw();
@@ -13,9 +13,23 @@ describe('The `changePossibilities` algorithm. (Your quirky boss collects rare, 
     expect(() => change(1)).to.throw(err);
     expect(() => change()).to.throw(err);
   });
-  it.only('Given an amount of money and an array of coin denominations, computes the number of ways to make the amount of money with coins of the available denominations', () => {
+
+  it('Given an amount of money and an array of coin denominations, computes the number of ways to make the amount of money with coins of the available denominations', () => {
+    let amount, denominations;
     amount = 4;  // (4¢)
     denominations = [1, 2, 3];  // (1¢, 2¢ and 3¢)
     expect(change(amount, denominations)).to.equal(4);
+
+    amount = 25;
+    denominations = [1, 5, 10, 25];
+    expect(change(amount, denominations)).to.equal(13);
+
+    amount = 0;
+    denominations = [1, 2, 3];
+    expect(change(amount, denominations)).to.equal(1);
+
+    amount = 1;
+    denominations = [2, 3, 4];
+    expect(change(amount, denominations)).to.equal(0);
   });
 });
